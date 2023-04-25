e53164e62a87591899e69bb6463c7c673279b3ed
MOINUDDIN ATHANIYA <athaniyamoinuddin@gmail.com>
2023-04-25 11:14:09 +0530

    row expended

--- lib/main.dart
+++ lib/main.dart
@@ -4,82 +4,45 @@ void main()
 {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(backgroundColor: Colors.grey,
-
-
-            title: Text('click button ')
-        ) ,
-        body:Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+        body:SafeArea(
+        child: Row(
           children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Container(
+            Container(
+              padding: EdgeInsets.all(20),
+              color: Colors.red,
+              child: Text('1'),
+            ),
+            Expanded(
+              flex: 7,
+                child: Container
+            (
 
+              padding: EdgeInsets.all(20),
+             // width: 70,
+              color: Colors.blue,
+              child: Text('2'),
+            )
+            ),
+            Expanded(
+              flex: 3,
+                child:Container
+                (
                   padding: EdgeInsets.all(20),
-                  color: Colors.red,
-                  child: Text('hello'),
-                ),
-                Container(
-                  padding: EdgeInsets.all(30),
-                  color: Colors.blue,
-
-                  child:  Text('hello'),
-                ),
-                Container(
-                  padding: EdgeInsets.all(35),
+                  // width: 200,
                   color: Colors.green,
-                  child: Text('hello'),
+                  child: Text('3'),
                 )
-
-              ],
-            ), //1
-            SizedBox(width:1),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(20),
-                  color: Colors.red,
-                  child: Text('Hello'),
-                ),
-                Container(
-                  padding: EdgeInsets.all(30),
-                  color: Colors.blue,
-                  child: Text('Hello'),
-                ),
-                Container(
-                  padding: EdgeInsets.all(35),
-                  color: Colors.green,
-                  child: Text('Hello'),
-                ),
-
-              ],
-            ),//
-
-
-
-
-
-
+            ),
 
           ],
-        ) ,
-        floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
-        floatingActionButton: FloatingActionButton(
-
-          onPressed: null,
-
-          child: Text('Click Hear'),
         ),
+        )
       ),
     );
   }
